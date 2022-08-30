d8e7ffb1fcbba1092084fee0f7c205debe037dc6
syouzan420 <syouzanponkikki@yahoo.co.jp>
2022-08-30 11:00:19 +0900

    Add files via upload

--- date.hs
+++ date.hs
@@ -1,23 +1,9 @@
-import Data.Time(utctDay, getCurrentTime)
-import Data.Time.Calendar.OrdinalDate(showOrdinalDate)
-import Data.Time.LocalTime(utc,TimeZone(..),getCurrentTimeZone,getZonedTime,utcToLocalTime,ZonedTime(..),LocalTime(..))
-import Data.Time.Format(formatTime,defaultTimeLocale)
+import Data.Time.LocalTime(getZonedTime,ZonedTime(zonedTimeToLocalTime),LocalTime(localDay))
 
-day :: IO (Int, Int, Int) 
+day :: IO String 
 day = do
   da <- show <$> localDay <$> zonedTimeToLocalTime <$> getZonedTime
-  return (read (take 4 da), read (take 2 (drop 5 da)), read (drop 8 da))
-
-day' :: IO String
-day' = showOrdinalDate <$> utctDay <$> getCurrentTime
-
-day'' :: IO String
-day'' = do
-  d <- show <$> utctDay <$> getCurrentTime
-  let da = drop 5 d
-  let mo = take 2 da
-  let dy = drop 3 da
-  return ("month: "++mo++" day: "++dy)
+  return ((take 4 da) ++ (take 2 (drop 5 da)) ++ (drop 8 da))
 
 wake :: String -> (Int, Int, Int)
 wake a = (read (take 4 a), read (take 2 (drop 4 a)), read (drop 6 a))
@@ -25,13 +11,29 @@ wake a = (read (take 4 a), read (take 2 (drop 4 a)), read (drop 6 a))
 daylist :: [Int]
 daylist = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-nin :: String -> Int
-nin a = let (y,m,d) = wake a
-         in if(m==1) then d
-                     else sum (take (m-1) daylist) + d
-
 uru :: Int -> Bool
 uru y = let r1 = mod y 4 == 0
             r2 = mod y 100 == 0
             r3 = mod y 400 == 0
          in r3 || (r1 && not r2)
+
+nen :: String -> Int
+nen a = let (y,m,d) = wake a
+            iuru = uru y
+         in if(m==1) then d
+                     else sum (take (m-1) daylist) + d 
+                          + (if (m>2 && iuru) then 1 else 0)
+
+daynow :: String -> IO Int
+daynow a = do
+  nday <- day
+  let (y,m,d) = wake a
+      (yn,_,_) = wake nday
+      fyday = 365 - nen a + (if (uru y) then 1 else 0)
+      lsday = nen nday 
+  return (fyday + mdday (y+1) (yn-1) + lsday)
+
+mdday :: Int -> Int -> Int
+mdday c t
+  | c==t = if (uru t) then 366 else 365
+  | otherwise = (if(uru c) then 366 else 365) + (mdday (c+1) t)
